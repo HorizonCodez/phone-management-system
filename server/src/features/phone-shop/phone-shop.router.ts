5aa8f45da34df2915e4e695203a1458c2661e570
Shashi Srinath <atshashisrianth@gmail.com>
2021-08-28 12:27:07 +0530

    create phone shop registration

--- server/src/features/phone-shop/phone-shop.router.ts
+++ server/src/features/phone-shop/phone-shop.router.ts
@@ -6,24 +6,54 @@ import {
     phoneShopRegisterValidationObject,
 } from './dto/phone-shop-register.dto';
 import phoneShopService from './phone-shop.service';
+import { imageUpload } from '../../lib/local-image-upload';
+import { HttpError } from '../../lib/http-error';
 
 const router = Router();
 
-router.post('/register', async (req, res, next) => {
-    const { error, value } = validate<PhoneShopRegisterDto>({
-        data: req.body,
-        schema: phoneShopRegisterValidationObject,
-    });
+router.post(
+    '/register',
+    imageUpload.fields([
+        { name: 'profileImage', maxCount: 1 },
+        { name: 'brImage', maxCount: 1 },
+    ]),
+    async (req, res, next) => {
+        const { error, value } = validate<PhoneShopRegisterDto>({
+            data: req.body,
+            schema: phoneShopRegisterValidationObject,
+        });
 
-    if (error) {
-        return next(new HttpValidationError(error));
-    }
+        if (error) {
+            return next(new HttpValidationError(error));
+        }
 
-    try {
-        return res.status(200).json(await phoneShopService.register(value));
-    } catch (e) {
-        return next(e);
+        /** checking images for errors **/
+        // validate images
+        // @ts-ignore
+        if (!req.files.profileImage || !Array.isArray(req.files.profileImage)) {
+            return next(new HttpError(400, 'Invalid profileImage'));
+        }
+        // @ts-ignore
+        if (!req.files.brImage || !Array.isArray(req.files.brImage)) {
+            return next(new HttpError(400, 'Invalid brImage'));
+        }
+
+        // @ts-ignore
+        const profileImageBuffer = req.files.profileImage[0]?.buffer;
+        // @ts-ignore
+        const brBuffer = req.files.brImage[0]?.buffer;
+
+        try {
+            return res.status(200).json(
+                await phoneShopService.register(value, {
+                    profileImage: profileImageBuffer,
+                    br: brBuffer,
+                })
+            );
+        } catch (e) {
+            return next(e);
+        }
     }
-});
+);
 
 export default router;
--- server/src/features/phone-shop/phone-shop.service.ts
+++ server/src/features/phone-shop/phone-shop.service.ts
@@ -2,8 +2,12 @@ import { PhoneShopRegisterDto } from './dto/phone-shop-register.dto';
 import prisma from '../../lib/prisma';
 import authService from '../auth/auth.service';
 import { HttpError } from '../../lib/http-error';
+import imageService from '../image/image.service';
 
-async function register(data: PhoneShopRegisterDto) {
+async function register(
+    data: PhoneShopRegisterDto,
+    images: { profileImage: unknown; br: unknown }
+) {
     // make sure city id is valid
     // todo: make a separate method on city feature
     const city = await prisma.city.findUnique({
@@ -15,28 +19,66 @@ async function register(data: PhoneShopRegisterDto) {
         throw new HttpError(404, 'Invalid city id', 'CityNotFound');
     }
 
-    // create app user
-    const user = await authService.register(
-        {
-            email: data.email,
-            password: data.password,
-        },
-        'Shop'
+    /** upload images **/
+    // add profile image
+    const uploadedProfileImage: any = await imageService.uploadImage(
+        images.profileImage,
+        'Profile'
     );
 
-    // create phone shop in database
-    await prisma.phoneShop.create({
-        data: {
-            userId: user.id,
-            shopName: data.shopName,
-            address: data.address,
-            phone: data.phone,
-            isVerified: false,
-            brId: 100,
-            cityId: 100,
-            profileImageId: 100,
-            coverImageId: 100,
-        },
+    // add br image
+    const uploadedBrImage: any = await imageService.uploadImage(
+        images.br,
+        'BR'
+    );
+
+    return await prisma.$transaction(async (prisma) => {
+        /** create app user **/
+        const user = await authService.register(
+            {
+                email: data.email,
+                password: data.password,
+            },
+            'Shop',
+            // @ts-ignore
+            prisma
+        );
+
+        /** create images **/
+        const profileImage = await imageService.create(
+            uploadedProfileImage.secure_url,
+            'Profile',
+            // @ts-ignore
+            prisma
+        );
+        const brImage = await imageService.create(
+            uploadedBrImage.secure_url,
+            'BR',
+            // @ts-ignore
+            prisma
+        );
+
+        /** create br **/
+        const br = await prisma.businessRegistration.create({
+            data: {
+                imgId: brImage.id,
+                registrationNumber: data.brNumber,
+            },
+        });
+
+        /** create phone shop in database **/
+        return prisma.phoneShop.create({
+            data: {
+                userId: user.id,
+                shopName: data.shopName,
+                address: data.address,
+                phone: data.phone,
+                isVerified: false,
+                cityId: data.cityId,
+                brId: br.id,
+                profileImageId: profileImage.id,
+            },
+        });
     });
 }
 
