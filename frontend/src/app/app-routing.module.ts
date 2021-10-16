31c95d7f5aa355e98ff2edb77c6f70558284f69f
Shashi Srinath <atshashisrinath@gmail.com>
2021-10-16 14:30:38 +0530

    setup routing for some sample pages

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { UserRegisterPageComponent } from './user-register-page/user-register-page.component';
+import { ShopRegisterPageComponent } from './shop-register-page/shop-register-page.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginPageComponent },
+  { path: 'user-register', component: UserRegisterPageComponent },
+  { path: 'shop-register', component: ShopRegisterPageComponent },
+  { path: '', component: HomePageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,12 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { TopbarComponent } from './topbar/topbar.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { UserRegisterPageComponent } from './user-register-page/user-register-page.component';
+import { ShopRegisterPageComponent } from './shop-register-page/shop-register-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SidebarComponent,
-    TopbarComponent
+    TopbarComponent,
+    HomePageComponent,
+    LoginPageComponent,
+    UserRegisterPageComponent,
+    ShopRegisterPageComponent
   ],
   imports: [
     BrowserModule,
